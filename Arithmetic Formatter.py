460246021af8051ef6b7f3a9042106f5072e38f8
ZokaKami <ruffy92@live.com>
2022-02-01 15:59:19 +0100

    Update Arithmetic Formatter.py

--- Arithmetic Formatter.py
+++ Arithmetic Formatter.py
@@ -2,10 +2,12 @@ import re
 
 
 def math(vals):
+
     for number in vals:
         x = re.search("\d+", number)
         y = re.search("[+,-]", number)
         z = re.search("\d+$", number)
+
         FirstNumber = x.group()
         Symbol = y.group()
         SecondNumber = z.group()
@@ -13,25 +15,20 @@ def math(vals):
         Result = str(Result)
 
         if(len(FirstNumber) < len(SecondNumber)):
-            print(FirstNumber.rjust(len(SecondNumber)+2))
-            print(Symbol, SecondNumber)
             Width = len(Symbol) + len(SecondNumber) + 1
-            print("-" * Width)
-            print(Result.rjust(Width), "\n")
+            print(FirstNumber.rjust(len(SecondNumber)+3), "\n", Symbol,
+                  SecondNumber, "\n", "-"*Width, "\n", Result.rjust(Width), "\n")
 
         elif(len(SecondNumber) < len(FirstNumber)):
-            print(FirstNumber.rjust(len(FirstNumber) + 1))
-            print(Symbol, z.group().rjust(len(FirstNumber)-1))
+
             Width = len(Symbol) + len(FirstNumber)
-            print("-" * Width)
-            print(Result.rjust(Width), "\n")
+            print(FirstNumber.rjust(len(FirstNumber)+2), "\n", Symbol,
+                  SecondNumber.rjust(len(FirstNumber)-1), "\n", "-"*Width, "\n", Result.rjust(Width), "\n")
 
         elif(len(SecondNumber) == len(FirstNumber)):
-            print(FirstNumber.rjust(len(FirstNumber)+2))
-            print(Symbol, z.group())
             Width = len(Symbol) + len(SecondNumber) + 1
-            print("-" * Width)
-            print(Result.rjust(Width), "\n")
+            print(FirstNumber.rjust(len(FirstNumber)+3), "\n", Symbol,
+                  SecondNumber, "\n", "-"*Width, "\n", Result.rjust(Width), "\n")
 
 
-math(["32 + 698", "3801 - 2", "45 + 43", "123 + 49", "111111111111 + 1"])
+math(["32 + 633", "2223801 - 2222", "45 + 433", "123 + 49", "1111111 + 1"])
